7fd1a5b1a6922bacdd8182ca919b23cdfc7ec388
unknown <zanetka281091@gmail.com>
2020-03-24 21:03:35 +0100

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  isCorrect = true;
+
+  isCorrect = isCorrect && str.length % 2 === 0;
+
+  let str2 = str;
+  
+  while (str2 !== '') {
+    let tempStr = str2;
+    
+    bracketsConfig.forEach(config => {
+        str2 = str2.split(config.join('')).join('');
+    })
+
+    if(str2 === tempStr) {
+      str2 = '';
+      isCorrect = false;
+    }
+  }
+
+
+  return isCorrect;
 }
+
+
+
